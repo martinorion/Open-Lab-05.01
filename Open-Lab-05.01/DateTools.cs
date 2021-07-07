d1ae1fba2b6b7f1a4408f9eb9764bd3440e3520c
Number One <71751917+martinorion@users.noreply.github.com>
2021-07-07 13:58:53 +0200

    Full version + alternative

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,18 +6,39 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-             int x;
-            if(year % 100 == 0)
+              int a;
+            if (year % 100 == 0)
             {
-                x = year / 100;
+                a = year / 100;
             }
             else
             {
-                x = year/100 + 1;
+                a = year / 100 + 1;
             }
 
-            string y = x.ToString();
-            return y;
+            string b = a.ToString();
+
+           // alternatíva int a = year % 100 == 0 ? year / 100 : year / 100 + 1;
+
+           
+            if(a == 11 || a == 12)
+            {
+                return b + "th century";
+            }
+            if (b.ToCharArray()[b.Length - 1] == '1')
+            {
+                return b + "st century";
+            }
+            if (b.ToCharArray()[b.Length - 1] == '2')
+                {
+                return b + "nd century";
+            }
+            if (b.ToCharArray()[b.Length - 1] == '3')
+            {
+                return b + "rd century";
+            }
+
+            return b + "th century";
         }
     }
 }
